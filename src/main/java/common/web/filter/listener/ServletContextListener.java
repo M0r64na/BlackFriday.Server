2d760c6daada03c774e8690fb379deb733e9a856
M0r64na <kiara.lazarova@yahoo.com>
2024-04-14 13:29:21 +0300

    :card_file_box: Added servlet context listener to initialize db on startup if needed

---
+++ src/main/java/common/web/filter/listener/ServletContextListener.java
@@ -0,0 +1,25 @@
+package common.web.filter.listener;
+
+import application.service.interfaces.IRoleService;
+import application.service.interfaces.IStatusService;
+import application.service.interfaces.IUserService;
+import common.factory.service.RoleServiceFactory;
+import common.factory.service.StatusServiceFactory;
+import common.factory.service.UserServiceFactory;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class ServletContextListener implements jakarta.servlet.ServletContextListener {
+    private final IRoleService roleService = RoleServiceFactory.getInstance();
+    private final IUserService userService = UserServiceFactory.getInstance();
+    private final IStatusService statusService = StatusServiceFactory.getInstance();
+
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        this.roleService.initializeRoles();
+        this.userService.initializeUsers();
+        this.statusService.initializeStatuses();
+    }
+}
